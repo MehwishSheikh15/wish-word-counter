f730fd1e8709ac22df2221697a065d1f061f3609
MehwishSheikh15 <mehwishsheikh451sheih@gmail.com>
2024-04-28 17:39:22 +0500

    check my word counter

---
+++ index.ts
@@ -0,0 +1,19 @@
+#! //usr/bin/env node
+
+import inquirer from "inquirer"
+
+const answers: {   
+    Sentence:string 
+} = await inquirer.prompt([
+    {
+        type:"input",
+        name:"Sentence",
+        message:"Enter your Sentence to count the world: "
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+
+
+console.log(words)
+console.log(`Your Sentence word count is ${words.length}`);
